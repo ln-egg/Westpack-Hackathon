b29e431472717ca59ca61cfac68e54fb6c6b48d4
Mary Nguyen <n11244747@qut.edu.au>
2024-07-04 11:45:49 +1000

    Init IExpensesDAO.java

--- src/main/java/com/example/westpackhackathon/model/IExpensesDAO.java
+++ src/main/java/com/example/westpackhackathon/model/IExpensesDAO.java
@@ -3,4 +3,7 @@ package com.example.westpackhackathon.model;
 import java.util.List;
 public interface IExpensesDAO {
     public void addExpenses (Expenses expenses);
+    public void removeExpenses (Expenses expenses);
+
+    public Expenses getExpenses (String Name);
 }
