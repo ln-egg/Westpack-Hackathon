87273dcd4ee7c995b1ad27af67b89c6930142d20
Mary Nguyen <n11244747@qut.edu.au>
2024-07-04 11:51:36 +1000

    Init IRevenueDAO.java

--- src/main/java/com/example/westpackhackathon/model/IRevenueDAO.java
+++ src/main/java/com/example/westpackhackathon/model/IRevenueDAO.java
@@ -1,4 +1,7 @@
 package com.example.westpackhackathon.model;
 public interface IRevenueDAO {
+    public void addRevenue (Revenue revenue);
+    public void removeRevenue(Revenue revenue);
 
+    public Revenue getRevenue(String Name);
 }
