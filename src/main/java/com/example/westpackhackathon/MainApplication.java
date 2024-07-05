4684980bdbca1b67a54074cda90e371dd20221ad
madeline tjawardi <madelinegt16@gmail.com>
2024-07-05 14:34:14 +1000

    Put Data into string

--- src/main/java/com/example/westpackhackathon/MainApplication.java
+++ src/main/java/com/example/westpackhackathon/MainApplication.java
@@ -15,42 +15,9 @@ public class MainApplication extends Application {
     public static final int WIDTH = 350;
     public static final int HEIGHT = 740;
 
-    private SqliteExpensesDAO expensesDAO;
-    private Expenses tExpense;
-
-    private SqliteRevenueDAO revenueDAO;
-    private Revenue tRevenue;
-
 
     @Override
     public void start(Stage stage) throws IOException {
-        expensesDAO = new SqliteExpensesDAO();
-        tExpense = new Expenses(0, Expenses.Type_Expenses.Optional, "", 0);
-
-        revenueDAO = new SqliteRevenueDAO();
-        tRevenue = new Revenue(0, Revenue.Type_Revenue.Transfer, "", 0);
-
-
-        // this is the sample how to access the total amount of expenses
-        int totalExpenses = expensesDAO.getTotalExpenses();
-        System.out.println("Total Expenses: " + totalExpenses);
-        /** this is sample how to access the total amount on different type of expenses
-         *  param type: Optional, Essential, Recreational
-         * */
-        int optionalExpenses = expensesDAO.getTotalExpensesOnType("Optional");
-        System.out.println("Total optional Expenses: " + optionalExpenses);
-
-        int totalRevenue = revenueDAO.getTotalRevenue();
-        System.out.println("Total Revenue: " + totalRevenue);
-
-        int payrollRevenue = revenueDAO.getTotalRevenueOnType("Payroll");
-        System.out.println("Total Payroll Revenue : " + payrollRevenue);
-
-
-        List<Expenses> tList = expensesDAO.getExpenseRecords("Optional");
-        System.out.println(tList.size());
-
-        //new Table();
 
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("homepage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), WIDTH, HEIGHT);
