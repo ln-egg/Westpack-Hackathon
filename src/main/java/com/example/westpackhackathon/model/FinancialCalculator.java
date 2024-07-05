fd48d4b28597640e9d2316517c36cb473d7be343
ln-egg <146163331+ln-egg@users.noreply.github.com>
2024-07-05 14:28:48 +1000

    fix bugs

--- src/main/java/com/example/westpackhackathon/model/FinancialCalculator.java
+++ src/main/java/com/example/westpackhackathon/model/FinancialCalculator.java
@@ -1,53 +1,53 @@
-package com.example.westpackhackathon.model;
-
-public class FinancialCalculator {
-    private SqliteRevenueDAO revenueDAO;
-    private SqliteExpensesDAO expensesDAO;
-
-    public FinancialCalculator(SqliteRevenueDAO revenueDAO, SqliteExpensesDAO expensesDAO) {
-        this.revenueDAO = revenueDAO;
-        this.expensesDAO = expensesDAO;
-    }
-
-    public double calculateProfit() {
-        double totalRevenue = revenueDAO.getTotalRevenue();
-        double totalExpenses = expensesDAO.getTotalAmount();
-        return totalRevenue - totalExpenses;
-    }
-
-    public double calculateWantsPercentage() {
-        double totalOptional = expensesDAO.OptionalPercentage();
-        double totalRecreational = expensesDAO.RecreationalPercentage();
-
-        return totalOptional + totalRecreational;
-    }
-
-    public double calculateNeedsPercentage() {
-        double totalEssential = expensesDAO.EssentialPercentage();
-        return totalEssential;
-    }
-
-    public double calculateSavingsPercentage() {
-        double totalRevenue = revenueDAO.getTotalRevenue();
-        double totalExpenses = expensesDAO.getTotalAmount();
-        return (totalRevenue - totalExpenses) / totalRevenue * 100;
-    }
-
-    public String budgetRuleResult() {
-        double wantsPercentage = calculateWantsPercentage();
-        double needsPercentage = calculateNeedsPercentage();
-        double savingsPercentage = calculateSavingsPercentage();
-
-        boolean followsRule = (needsPercentage == 50) && (wantsPercentage == 30) && (savingsPercentage == 20);
-        boolean closeToRule = (Math.abs(needsPercentage - 50) <= 10) && (Math.abs(wantsPercentage - 30) <= 10) && (Math.abs(savingsPercentage - 20) <= 10);
-
-        /* CHANGE THIS ACCORDING TO THE DIALOGUE*/
-        if (followsRule) {
-            return "You follow the budget rule";
-        } else if (closeToRule) {
-            return "You are close to reaching the budget rules";
-        } else {
-            return "You do not follow the budget rule";
-        }
-    }
-}
+//package com.example.westpackhackathon.model;
+//
+//public class FinancialCalculator {
+//    private SqliteRevenueDAO revenueDAO;
+//    private SqliteExpensesDAO expensesDAO;
+//
+//    public FinancialCalculator(SqliteRevenueDAO revenueDAO, SqliteExpensesDAO expensesDAO) {
+//        this.revenueDAO = revenueDAO;
+//        this.expensesDAO = expensesDAO;
+//    }
+//
+//    public double calculateProfit() {
+//        double totalRevenue = revenueDAO.getTotalRevenue();
+//        double totalExpenses = expensesDAO.getTotalAmount();
+//        return totalRevenue - totalExpenses;
+//    }
+//
+//    public double calculateWantsPercentage() {
+//        double totalOptional = expensesDAO.OptionalPercentage();
+//        double totalRecreational = expensesDAO.RecreationalPercentage();
+//
+//        return totalOptional + totalRecreational;
+//    }
+//
+//    public double calculateNeedsPercentage() {
+//        double totalEssential = expensesDAO.EssentialPercentage();
+//        return totalEssential;
+//    }
+//
+//    public double calculateSavingsPercentage() {
+//        double totalRevenue = revenueDAO.getTotalRevenue();
+//        double totalExpenses = expensesDAO.getTotalAmount();
+//        return (totalRevenue - totalExpenses) / totalRevenue * 100;
+//    }
+//
+//    public String budgetRuleResult() {
+//        double wantsPercentage = calculateWantsPercentage();
+//        double needsPercentage = calculateNeedsPercentage();
+//        double savingsPercentage = calculateSavingsPercentage();
+//
+//        boolean followsRule = (needsPercentage == 50) && (wantsPercentage == 30) && (savingsPercentage == 20);
+//        boolean closeToRule = (Math.abs(needsPercentage - 50) <= 10) && (Math.abs(wantsPercentage - 30) <= 10) && (Math.abs(savingsPercentage - 20) <= 10);
+//
+//        /* CHANGE THIS ACCORDING TO THE DIALOGUE*/
+//        if (followsRule) {
+//            return "You follow the budget rule";
+//        } else if (closeToRule) {
+//            return "You are close to reaching the budget rules";
+//        } else {
+//            return "You do not follow the budget rule";
+//        }
+//    }
+//}
